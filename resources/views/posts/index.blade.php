01b5346eadcba981eb3ea2a90e9d7401c4740c69
sky200079191-dev <sky200079191@gmail.com>
2026-02-10 00:24:02 +0900

    fix: goodボタン追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -69,6 +69,23 @@
                         </div>
                     @endif
                 </div>
+                <div class="mt-2">
+                    <form action="{{ route('posts.like', $post) }}" method="POST" style="display: inline;">
+                        @csrf
+                        <input type="hidden" name="is_good" value="1">
+                        <button type="submit" class="btn btn-sm btn-outline-primary">
+                            👍 {{ $post->likes()->where('is_good', true)->count() }}
+                        </button>
+                    </form>
+
+                    <form action="{{ route('posts.like', $post) }}" method="POST" style="display: inline;">
+                        @csrf
+                        <input type="hidden" name="is_good" value="0">
+                        <button type="submit" class="btn btn-sm btn-outline-danger">
+                            👎 {{ $post->likes()->where('is_good', false)->count() }}
+                        </button>
+                    </form>
+                </div>
             @endforeach
             <div class='mt-8 px-4'>
                 {{ $posts->links() }}
